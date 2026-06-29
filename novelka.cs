82704fbff9efbcf4751600566e5307189f61c8e8
Chebbupula <vanek24611@gmail.com>
2026-06-29 23:37:47 +0300

    почти готов второй диалог

--- novelka.cs
+++ novelka.cs
@@ -7,7 +7,30 @@ namespace novelka
     {
         static void Main()
         {
-            System.Console.WriteLine("Привет никита, надеюсь ты прочитал описание novelka.cs.Я обязуюсь, такого больше не будет(мэйби).(рекомендую не играть пока не прочитаешь)\nПриветствую в новелке, она про поездеку в голубы.)");
+            System.Console.WriteLine("Привет никита, надеюсь ты прочитал описание novelka.cs.Я обязуюсь, такого больше не будет(мэйби).(рекомендую не играть пока не прочитаешь)\nПриветствую в новелке, она про поездеку в голубы.\nВ данный момент вы на Мостовском Автовокзале.Вы зашли в автобус, выберите с кем сесть\n1.Бабушка\n2.Дедушка\n3.Женщина с грудным ребенком\n4.Красивая альтушка\n5.Женщина с псом");
+            string UserChoice = Console.ReadLine();
+            if (1 == Convert.ToInt32(UserChoice))
+            {
+                System.Console.WriteLine("Вы подсели к бабушке.Она начала разговор\nМилок, ты до куда\nДо голубов\nА ты чей? Что ответите\n1.Назвать фамилию\n2.Соврать.");
+                string FirstUserChoice = Console.ReadLine();
+                if (1 == Convert.ToInt32(FirstUserChoice))
+                {
+                    System.Console.WriteLine("О так она моя подружка.Всю поездку вы слушали рассказы из детства связаные с вашей бабушкой.Вам было интересно.Хорошая концовка.");
+                }
+                if (2 == Convert.ToInt32(FirstUserChoice))
+                {
+                    System.Console.WriteLine("Понятно\nВсю дальнейшую дорогу вы проехали в тишине.Нормальная концовка");
+                }
+            }
+            if (2 == Convert.ToInt32(UserChoice))
+            {
+                System.Console.WriteLine("Вы сели к дедушке\nТы куда едешь? Что ответие\n1.Сделать вид что не слышите\n2.Ответить");
+                string SecondUserChoice = Console.ReadLine();
+                if (1 == Convert.ToInt32(SecondUserChoice))
+                {
+                    System.Console.WriteLine("Ну ладно\n");//TODO
+                }
+            }
         }
     }
 }
