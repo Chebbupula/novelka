d4accdb7af5e354cb8c63e4d5a6767dd1c90c9ed
Chebbupula <vanek24611@gmail.com>
2026-06-29 22:10:19 +0300

    fix

---
+++ novelka.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net.Mail;
+
+namespace novelka
+{
+    class Programm
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Привет никита, надеюсь ты прочитал описание novelka.cs.Я обязуюсь, такого больше не будет(мэйби).(рекомендую не играть пока не прочитаешь\nПриветствую в новелке, она про поездеку в голубы.)");
+        }
+    }
+}
